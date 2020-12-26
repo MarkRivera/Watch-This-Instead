d94a18aaf136242426cb377ae3243345e72faffd
Mark Rivera <Mriver1991@gmail.com>
2020-12-25 21:19:21 -0500

    added genre query to middle ware

--- data/server.js
+++ data/server.js
@@ -24,38 +24,41 @@ server.use(cors());
 server.use(helmet());
 server.use(morgan("dev"));
 
-try {
-  const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}&language=en-US`;
-
-  (async () => {
-    // Get Genres Here:
-    const { data } = await axios.get(genreUrl);
-    const { genres } = data;
-
-    // Check each genre and see if it exists in my database:
-    genres.forEach(async genre => {
-      const isInDatabase = await checkIfExists(genre.id);
-
-      if (isInDatabase.length === 0) {
-        // Create genre instance:
-        const currentGenre = genre.name;
-
-        const genreObject = {
-          tmdbId: genre.id,
-          genre: currentGenre,
-          totalNumberOfUsers: 0,
-        };
-
-        add(genreObject, genre.id);
-      }
-    });
-  })();
-} catch (error) {
-  next(error);
-}
-
 server.use("/api/users", UsersRouter);
 server.use("/api/movies", MoviesRouter);
+
+server.use(async function (req, res) {
+  try {
+    const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}&language=en-US`;
+
+    (async () => {
+      // Get Genres Here:
+      const { data } = await axios.get(genreUrl);
+      const { genres } = data;
+
+      // Check each genre and see if it exists in my database:
+      genres.forEach(async genre => {
+        const isInDatabase = await checkIfExists(genre.id);
+
+        if (isInDatabase.length === 0) {
+          // Create genre instance:
+          const currentGenre = genre.name;
+
+          const genreObject = {
+            tmdbId: genre.id,
+            genre: currentGenre,
+            totalNumberOfUsers: 0,
+          };
+
+          add(genreObject, genre.id);
+        }
+      });
+    })();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 server.get("/", (req, res) => {
   try {
