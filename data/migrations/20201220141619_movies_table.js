d3d3808ee32c8ce1f3cc0f9a1d0481a34f1654e5
Mark Rivera <Mriver1991@gmail.com>
2020-12-29 13:21:13 -0500

    changed knex migrations

--- data/migrations/20201220141619_movies_table.js
+++ data/migrations/20201220141619_movies_table.js
@@ -2,9 +2,9 @@ exports.up = async function (knex) {
   await knex.schema.createTable("movies", table => {
     table.increments("id").unsigned();
     table.integer("tmdbId").notNullable();
-    table.string("title").notNullable();
+    table.string("title", 1000).notNullable();
     table.integer("year").unsigned();
-    table.string("description");
+    table.string("description", 1000);
     table.unique(["tmdbId"]);
   });
 };
