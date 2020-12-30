957b179ead6dadbf5b1ff1b02b649329cb107f41
Mark Rivera <Mriver1991@gmail.com>
2020-12-30 00:53:05 -0500

    trying changes

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,6 @@
+var pg = require("pg");
+pg.defaults.ssl = true;
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -37,7 +40,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: `${process.env.DATABASE_URL}sslmode=require`,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
